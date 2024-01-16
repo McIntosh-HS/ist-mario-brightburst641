c3692e9b321a706ef7b290792a1aaf05c2a5a1b6
brightburst641 <2027relova.kenzo@fcboe.org>
2024-01-16 14:36:49 -0500

    Completed asciiart assignment

---
+++ asciiart.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("          |                     |\n");
+	printf("  ____    |                     |        ___ \n");
+	printf(" |    |   |                  |  |   |   |   | \n");
+	printf(" |    |   |             =O   |  |   |   |   | \n");
+	printf("      |   |  |               |  |   |   |   |\n");
+	printf("      |   |  |                  |   |   |   |\n");
+	printf("      |   |  |                  |   |   |___|    \n");
+	printf("          |                     |    \n");
+	printf("          |                     |    \n");
+	printf("          |                     |     \n");
+	return 0;
+}
